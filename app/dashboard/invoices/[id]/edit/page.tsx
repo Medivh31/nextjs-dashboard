072efd10fb6a07cb51f303a3f9533226827f70e4
曲成哲 <qu031031@163.com>
2025-09-25 18:36:24 +0800

    metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,10 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 //除了 searchParams 之外，页面组件还接受一个名为 params 的 prop，您可以使用它来访问 id
 export default async function Page(props:{params:Promise<{id:string}>}) {
     const params = await props.params;
